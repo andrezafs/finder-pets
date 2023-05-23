3734f4ab782c4c66380294db1819410b3f217949
Andreza Ferreira <andrezafs.rr@gmail.com>
2023-05-22 21:27:07 -0300

    fix: 🐛 adjust props and add forwardRed

--- src/components/InputAccont/index.tsx
+++ src/components/InputAccont/index.tsx
@@ -1,30 +1,30 @@
-export function InputAccount({
-  htmlFor,
-  type,
-  label,
-  name,
-  id,
-}: {
+import { type InputHTMLAttributes, forwardRef } from "react";
+
+interface InputAccountProps extends InputHTMLAttributes<HTMLInputElement> {
   htmlFor: string;
-  type: string;
   label: string;
   name: string;
   id: string;
-}) {
-  return (
-    <>
-      <label
-        htmlFor={htmlFor}
-        className="font-inter text-sm font-semibold text-brown"
-      >
-        {label}
-      </label>
-      <input
-        type={type}
-        name={name}
-        id={id}
-        className="border-solid border border-gray-100 rounded-lg outline-none py-3 px-4 w-80"
-      />
-    </>
-  );
 }
+
+export const InputAccount = forwardRef<HTMLInputElement, InputAccountProps>(
+  ({ htmlFor, label, ...props }, ref) => {
+    return (
+      <>
+        <label
+          htmlFor={htmlFor}
+          className="font-inter text-sm font-semibold text-brown"
+        >
+          {label}
+        </label>
+        <input
+          ref={ref}
+          className="border-solid border border-gray-100 rounded-lg outline-none py-3 px-4 w-80"
+          {...props}
+        />
+      </>
+    );
+  }
+);
+
+InputAccount.displayName = "InputAccount";
