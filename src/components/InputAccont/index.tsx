cc269d5793c64f0a00ec2e0f98e3a548ef775600
Andreza Ferreira <andrezafs.rr@gmail.com>
2023-05-23 10:08:21 -0300

    fix: 🐛 adjust message validation

--- src/components/InputAccont/index.tsx
+++ src/components/InputAccont/index.tsx
@@ -17,13 +17,14 @@ export const InputAccount = forwardRef<HTMLInputElement, InputAccountProps>(
         >
           {label}
         </label>
+
         <input
           ref={ref}
           id={id}
           className="border-solid border border-gray-100 rounded-lg outline-none py-3 px-4 w-80"
           {...props}
         />
-        {error ?? <span className="text-red-500">{error}</span>}
+        {error != null && <span className="text-red-600 ">{error}</span>}
       </>
     );
   }
