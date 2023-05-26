bc541250faf71e96b1511d3ab76f63764758a554
Andreza Ferreira <andrezafs.rr@gmail.com>
2023-05-26 12:18:40 -0300

    feat: ✨ create separator component

---
+++ src/components/Separator/index.tsx
@@ -0,0 +1,9 @@
+export function Separator() {
+  return (
+    <div className="flex justify-between items-center gap-5 mt-6 ">
+      <div className="border-solid border border-gray-400 w-32 h-0"></div>
+      <span className="font-inter font-semibold text-sm text-gray-400">OR</span>
+      <div className="border-solid border border-gray-400 w-32 h-0"></div>
+    </div>
+  );
+}
